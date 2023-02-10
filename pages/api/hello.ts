17da50482616122b6615a6726f0da16ba5dc8b5f
CodeSandbox <support@codesandbox.io>
2023-02-10 05:15:44 +0000

    Initial commit

---
+++ pages/api/hello.ts
@@ -0,0 +1,31 @@
+/* eslint-disable */
+
+import type { NextApiRequest, NextApiResponse } from "next";
+import Client from "gpt-free";
+
+type Data = {
+  error?: string;
+  prompt?: string;
+  response?: string;
+};
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  const client = new Client();
+  const { prompt }: any = req.query;
+
+  if (!prompt) {
+    res.status(403).json({
+      error: "Prompt not defined",
+    });
+  }
+
+  const response = await client.model("chat").getCompleteResponse(prompt);
+
+  res.status(200).json({
+    prompt,
+    response,
+  });
+}
